05b7b12dd7bd24780d21e2dd5fbd83df1ddaa84e
Paul Prevel <prevel@biorobpclab9.epfl.ch>
2019-03-12 14:18:58 +0100

    refactored with file_utils

--- src/data_analysis/import_run.py
+++ src/data_analysis/import_run.py
@@ -5,40 +5,22 @@ import matplotlib.pyplot as plt
 import re
 import pandas as pd
 
+import utils.file_utils as fu
+
 CSV_SAVE_DIR="/data/prevel/repos/biorob-semesterproject/data"
 
 
 
-def file_list(path,recursive=False):
-	is_dir=os.path.isdir(path)
-	is_file=os.path.isfile(path)
-	if is_dir:
-		print ("Importing files in dir",path)
-		return [os.path.join(path,f) for f in os.listdir(path) if os.path.isfile(os.path.join(path,f))]
-	elif is_file:
-		return [path] # formating to list to avoid iter errors
-	else:
-		print("Nothing in ", path)
-		return None
-def concat_field(file_name,header):
-	concat_lst=[]
-	for field in header:
-		if field=="\n":
-		#	print("Removing line break")
-			pass
-		else:
-			print(file_name)
-			concat_lst.append(file_name+"_"+field)
-	return concat_lst
 def import_run(path,verbose=False,save_to_single=True,save_name="default_name",save_path="."):
 	first_call=True
-	for file_path in file_list(path):
+	
+	for file_path in fu.file_list(path):
 
 		array,header=import_file(file_path,verbose=False)
 		if save_to_single:
 			file=os.path.basename(file_path) # just the file name
 			file=os.path.splitext(file)[0] # remove extension
-			fields=concat_field(file, header)
+			fields=fu.concat_field(file, header)
 			new_name=export_single_file(array,fields,save_name,save_path,first_call,verbose)
 			first_call=False
 	return
@@ -63,47 +45,16 @@ def import_file(path,verbose=False):
 
 	return array,header
 
-def assert_file_exists(file_path,should_exist):
-	"""
-	Checks if a file exists. If it is the case and the file should not exist, propose to replace name (recursive)
-
-	-> WIP, doesn't update name
-
-	"""
-	if os.path.isfile(file_path):
-		if should_exist:
-			return
-		else:
-			print("A file already exists at location",file_path)
-			replace=input ("Replace file : Y/N\n")
-			if replace=="N":
-				new_name=input("Insert a new file name (foo.csv) to create a new file, or N to exit:\n")
-				if new_name=="N":
-					sys.exit()
-				else:
-					cdir=os.path.dirname(file_path)
-					assert_file_exists(os.path.join(cdir,new_name), should_exist=False)
-			elif replace=="Y":
-				print("Deleting file",file_path)
-				os.remove(file_path)
-				open(file_path, "w")
-			return
-	else:
-		if should_exist:
-			open(file_path, "w")
-			return
-		else:
-			return
 
 def export_single_file(data,fields,outputname,outputdir,first_call,verbose=False):
 
 	file_path=os.path.join(outputdir,outputname)
 	file_path=file_path+".csv"
 	if first_call:
-		assert_file_exists(file_path, should_exist=False)
+		fu.assert_file_exists(file_path, should_exist=False)
 		df=pd.DataFrame()
 	else:
-		assert_file_exists(file_path, should_exist=True)
+		fu.assert_file_exists(file_path, should_exist=True)
 		df=pd.read_csv(file_path)
 		
 	length,width=data.shape
@@ -135,9 +86,3 @@ if __name__=="__main__":
 	else:
 		print ("Args are ", sys.argv,"multiple run import not implemented yet")
 		
-	
-
-
-
-
-
---
+++ src/utils/file_utils.py
@@ -0,0 +1,86 @@
+import os
+import sys
+from shutil import rmtree
+
+def file_list(path,recursive=False,file_format="any_file"):
+	print("Listing files in",path)
+	print(os.listdir(path))
+	if file_format=="any_file":
+		is_file=os.path.isfile(path)
+		is_dir=os.path.isdir(path)
+	else:
+		path=[f for f in os.listdir(path) if f.endswith(file_format)]
+		if len(path)==1:
+			is_file=True
+			path=path[0] # unfold path (result from filter was a list)
+		elif len(path)==0:
+			print("No file with extension",file_format,"in",path)
+		else:
+			is_dir=True
+	if is_dir:
+		if file_format=="any_file":
+			return [os.path.join(path,f) for f in os.listdir(path) if os.path.isfile(os.path.join(path,f))]
+		else: # already listed
+			return path
+	elif is_file:
+		return [path] # formating to list to avoid iter errors
+	else:
+		print("Nothing in ", path)
+		return None
+def assert_dir(dir_path,should_be_empty=True):
+	if os.path.isdir(dir_path):
+		if file_list(dir_path)is not None and should_be_empty:
+			msg="Directory:"+str(dir_path)+"is not empty. Remove files? Y/N \n"
+			remove=input(msg)
+			if remove=="Y":
+				rmtree(dir_path)
+		return
+	else:
+		print("Creating directory",dir_path)
+		os.makedirs(dir_path) 
+		return
+		
+
+
+def assert_file_exists(file_path,should_exist):
+	"""
+	Checks if a file exists. If it is the case and the file should not exist, propose to replace name (recursive)
+
+	-> WIP, doesn't update name
+
+	"""
+	if os.path.isfile(file_path):
+		if should_exist:
+			return
+		else:
+			print("A file already exists at location",file_path)
+			replace=input ("Replace file : Y/N\n")
+			if replace=="N":
+				new_name=input("Insert a new file name (foo.csv) to create a new file, or N to exit:\n")
+				if new_name=="N":
+					sys.exit()
+				else:
+					cdir=os.path.dirname(file_path)
+					assert_file_exists(os.path.join(cdir,new_name), should_exist=False)
+			elif replace=="Y":
+				print("Deleting file",file_path)
+				os.remove(file_path)
+				open(file_path, "w")
+			return
+	else:
+		if should_exist:
+			print("Error, file at",file_path,"should exist!!")
+			return
+		else:
+			open(file_path, "w")
+			return
+def concat_field(file_name,suffix):
+	concat_lst=[]
+	for field in suffix:
+		if field=="\n":
+		#	print("Removing line break")
+			pass
+		else:
+			print(file_name)
+			concat_lst.append(file_name+"_"+field)
+	return concat_lst
