ab928d9a97c25fc6e929ef83177f5b5637beebd4
Paul Prevel <prevel@biorobpclab9.epfl.ch>
2019-03-18 12:50:46 +0100

    run analysis started

--- src/data_analysis/process_run.py
+++ src/data_analysis/process_run.py
@@ -6,6 +6,32 @@ Inputs :
 	output_file_path
 	plot
 
+EXAMPLE
+python process_run.py \
+/data/prevel/trial/result/param1/world1 \
+/data/prevel/trial/result/param1/ \
+all
+
 """
+import pandas as pd
+import sys
+import utils.file_utils as fu
+
+
+def compute_df(raw_file,process_params):
+
+	data_in=pd.read_csv(open(raw_file))
+	data_out=pd.DataFrame(data_in.index)
+	print (data_in)
+	print(data_out)
 if __name__ == '__main__':
-	print("TODOOO")
+	if len(sys.argv)>=4:
+		raw_path=sys.argv[1]
+
+		out_path=sys.argv[2]
+		raw_files=fu.file_list(raw_path,file_format=".csv")
+		process_params=sys.argv[3:]
+		for raw in raw_files:
+			df=compute_df(raw,process_params)
+		#export_file(df,out_file)
+		
