31c7da597b07802563e56a1102f064ab05ec5083
Paul Prevel <paul.prevel@epfl.ch>
2019-03-26 18:39:26 +0100

    run launcher class, handles individuals/generation. No online eval yet

--- src/run_batch_controller/run_launcher.py
+++ src/run_batch_controller/run_launcher.py
@@ -44,7 +44,7 @@ from shutil import copy,copyfile
 import os
 import subprocess
 from run_batch_controller.generate_paramfile_range import gen_all_file
-from run_batch_controller.unfold_param import complete_files
+import run_batch_controller.unfold_param as up
 """
 
 """
@@ -57,7 +57,7 @@ ROOT_RESULT_DIR="/data/prevel/runs"
 # Path relative to H2D_SRC_DIR, only change if directory stucture is different
 CONTROLLER_RPATH="webots/controllers/GeyerReflex"
 PARAMFILE_RPATH="modeling/configFiles/Controllers/current.yaml"
-REFERENCE_PARAMFILE_RPATH="modeling/configFiles/Controllers/geyer-reflex1.yaml"
+REFERENCE_PARAMFILE_RPATH="modeling/configFiles/Controllers/geyer-reflex_sym1.yaml" # changed to symetrical
 WORLD_RPATH="webots/worlds/current.wbt"
 
 CONTROLLER_ABSPATH=os.path.join(H2D_SRC_DIR,CONTROLLER_RPATH)
@@ -66,11 +66,104 @@ REFERENCE_PARAMFILE_ABSPATH=os.path.join(H2D_SRC_DIR,REFERENCE_PARAMFILE_RPATH)
 WORLD_ABSPATH=os.path.join(H2D_SRC_DIR,WORLD_RPATH)
 SIM_OUTPUTDIR_RPATH=os.path.join(CONTROLLER_ABSPATH,"Raw_files")
 
+
+class runLauncher:
+	world_counter=1
+	individual_counter=1
+	max_folds=1
+	def __init__(self,args):
+		self.time_start=time.time()
+		mode=args[0]
+		if len(args)==4:
+			self.trial_dir=args[3]
+		elif len(args)==3:
+			self.trial_dir=os.path.join(ROOT_RESULT_DIR,time.strftime("%j_%H:%M"))
+		
+		if mode=="param_fixed_values":
+			"""
+			python run_launcher.py 	param_fixed_values /data/prevel/params/test_range_GSOL.yaml /data/prevel/trial/worlds_folder 
+			"""
+			param_values_file=args[1]
+			tmp_folded_dir=os.path.join(self.trial_dir,"tmp_folded")
+			nested_runs=True
+			gen_all_file(param_values_file,tmp_folded_dir,standalone=True,nested=nested_runs)
+			if nested_runs:
+				self.gens=[]
+				param_dirs=fu.dir_list(tmp_folded_dir, "")
+				for param in param_dirs:
+					self.gens.append(fu.file_list(param,file_format=".yaml"))
+			gen_id=None
+		elif mode=="param_folder":
+			param_dir==args[1]
+			self.individuals=fu.file_list(self.param_dir,file_format=".yaml")
+		elif mode=="single_run":
+			param_file=args[1]
+			self.check_run(param_file)
+			return
+		fu.assert_dir(self.trial_dir)
+		world_dirpath=args[2]
+		self.worlds=fu.file_list(world_dirpath,file_format=".wbt")
+
+		gen_count=1
+		for gen in self.gens:
+			self.individuals=gen
+			gen_id="param"+str(gen_count)
+			self.run_gen(gen_id)
+			gen_count+=1
+	def run_gen(self,gen_id):
+		if gen_id is not None:
+			self.cdir=os.path.join(self.trial_dir,gen_id)
+			fu.assert_dir(self.cdir,should_be_empty=True)
+			self.gen_dir=os.path.join(self.trial_dir,gen_id)
+
+			print ("\n*************\t",gen_id,"\t************* \n")
+		else:
+			self.gen_dir=self.trial_dir
+			print("\n**************************\n")
+		tot_ind=len(self.individuals)
+		self.individual_counter=1
+		for ind in self.individuals:
+			print("Individual:\t",self.individual_counter,"\n")
+			self.world_counter=1
+			self.cdir=os.path.join(self.gen_dir,"ind"+str(self.individual_counter))
+
+			fu.assert_dir(self.cdir,should_be_empty=True)
+			folded_path=os.path.join(self.cdir,"meta.yaml")
+			copy(ind, folded_path) 
+			unfolded_path=os.path.join(self.cdir,"parameters.yaml")
+		
+			up.create_file(ind,REFERENCE_PARAMFILE_ABSPATH,PARAMFILE_ABSPATH,copy_path=unfolded_path)
+			for world in self.worlds:
+				if len(self.worlds)>1:
+					print("\tWorld:\t", self.world_counter,"\n")
+				self.fold_counter=0
+				for self.fold_counter in range(self.max_folds):
+					self.single_run(world)
+					self.fold_counter+=1
+				self.world_counter+=1
+			self.individual_counter+=1
+		print("\n**************************")
+
+	def single_run(self,world_file):
+		if self.fold_counter==0:
+			copyfile(world_file, WORLD_ABSPATH)
+		subprocess.run(["webots", "--mode=fast", "--batch","--minimize", WORLD_ABSPATH])
+		run_name="raw_w"+str(self.world_counter)+"f"+str(self.fold_counter+1)
+		imp.import_run(SIM_OUTPUTDIR_RPATH,save_path=self.cdir,save_name=run_name)
+	
+	def check_run(self,param_file):
+		if up.compare_files(REFERENCE_PARAMFILE_ABSPATH,test_file=param_file,verbose=True):
+			copyfile(param_file, PARAMFILE_ABSPATH)
+		else:
+			up.create_file(param_file,REFERENCE_PARAMFILE_ABSPATH,PARAMFILE_ABSPATH)
+		subprocess.run(["webots", "--mode=realtime", "--batch","--fullscreen", WORLD_ABSPATH])
+
 def launch_run(parameter_file=None,world_file=None):
 
 	subprocess.run(["webots", "--mode=fast", "--batch","--minimize", WORLD_ABSPATH])
 	#print("RUUUNNN")
-
+def gen_param(param_range_file,param_dst):
+	gen_all_file(param_range_file, param_dst,standalone=True)
 def gen_param_and_launch(param_range_file,world_dirpath,run_dir):
 	print(run_dir)
 	fu.assert_dir(run_dir)
@@ -124,6 +217,8 @@ def launch_param_dir(parameter_dirpath,world_dirpath,result_dir,nfolds=1,verbose
 		param_counter=param_counter+1
 
 if __name__ == '__main__':
+	runLauncher(sys.argv[1:])
+	"""
 	mode=sys.argv[1]
 
 	if mode=="param_dir":
@@ -140,14 +235,13 @@ if __name__ == '__main__':
 
 		launch_param_dir(parameter_dirpath, world_dirpath, result_dir, nfolds)
 	elif mode=="param_file_range":
-		"""
-		python run_launcher.py \
+				python run_launcher.py \
 		param_file_range \
 		/data/prevel/params/test_min_max.yaml \
 		/data/prevel/trial/worlds_folder \
 		/data/prevel/trial/result (optional, otherwise in /data/prevel/runs/date)
 
-		"""
+		
 		param_range_file=sys.argv[2]
 		world_dirpath=sys.argv[3]
 		if len(sys.argv)==5:
@@ -155,7 +249,7 @@ if __name__ == '__main__':
 		elif len(sys.argv)==4:
 			run_dir=os.path.join(ROOT_RESULT_DIR,time.strftime("%j_%H:%M"))
 		gen_param_and_launch(param_range_file,world_dirpath, run_dir)
-
+	"""
 
 
 
