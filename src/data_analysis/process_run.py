4df135e9c0de30a83ec7f5f8a54da93f19c5fe0a
Paul Prevel <paul.prevel@epfl.ch>
2019-05-24 00:30:11 +0200

    runProcess class

--- src/data_analysis/process_run.py
+++ src/data_analysis/process_run.py
@@ -22,6 +22,7 @@ import utils.file_utils as fu
 import matplotlib.pyplot as plt
 import data_analysis.event_detection as ed
 from utils.plot_utils import plot_mean_std_fill
+
 TIME_STEP=1e-3 #ms
 SAVEPATH="/data/prevel/runs/figures"
 
@@ -42,7 +43,7 @@ class reference_compare:
 	rep_strides={}
 
 	def __init__(self,kind,files=None):
-		if kind=='florin':
+		if kind=='cpp_to_python': # cpp to raw  !
 			""" directions :
 				ankle -
 				knee +
@@ -53,7 +54,7 @@ class reference_compare:
 			contact=pd.read_csv(contact_file,sep=" ")
 			joints=pd.read_csv(joints_file,sep=" ")
 			self.set_repstrides_florin(contact, joints)
-		elif kind=='winter':
+		elif kind=='winter_to_python': # winter to raw (python) !
 			""" directions :
 				ankle + 
 				knee +
@@ -63,16 +64,16 @@ class reference_compare:
 			win_df=pd.read_csv(winter_file)
 			self.set_repstrides_winter(win_df)
 
-		elif kind=='winter_cpp': # winter to cpp (florin) !
+		elif kind=='winter_to_cpp': # winter to cpp (florin) !
 			""" directions :
 				ankle -
 				knee +
 				hip -
 			""" 
-			winter_file=files[0]
+			winter_file=files
 			win_df=pd.read_csv(winter_file)
 			self.set_repstrides_winter_cpp(win_df)
-		elif kind=='raw':
+		elif kind=='python_to_python': # raw to raw (florin) !
 			""" directions :
 				ankle + 
 				knee +
@@ -147,8 +148,9 @@ def contact_df(full_df):
 	contact=full_df.filter(like="footfall1")
 	contact.columns=["left","right"]
 	return contact
-def metric_df(raw_file,objectives_file,ref_cmp=None,verbose=True):
 
+def metric_df(raw_file,objectives_file,ref_cmp=None,verbose=True):
+	#raise DeprecationWarning
 	raw_df=pd.read_csv(open(raw_file))
 	# Metrics computed during the run
 	metrics=pd.read_csv(open(objectives_file))
@@ -167,7 +169,7 @@ def metric_df(raw_file,objectives_file,ref_cmp=None,verbose=True):
 
 
 def metric_dct_cpp(raw_file,ref_cmp=None,verbose=False):
-
+	#raise DeprecationWarning
 	raw_df=pd.read_csv(open(raw_file))
 	# Metrics computed during the run
 
@@ -189,7 +191,7 @@ def metric_dct_cpp(raw_file,ref_cmp=None,verbose=False):
 	return metrics
 
 def process_cpp(ind_dir,ref_cmp=None,save=True,verbose=False):
-
+	#raise DeprecationWarning
 	raws=fu.file_list(ind_dir,file_format=".csv",pattern="raw")
 
 	raws=fu.assert_one_dim(raws,critical=False)
@@ -199,10 +201,13 @@ def process_cpp(ind_dir,ref_cmp=None,save=True,verbose=False):
 	if save:
 		fu.assert_dir(ind_dir,should_be_empty=False)
 		save_path=os.path.join(ind_dir,"processed.csv")
+		print("Saving to",save_path)
 		df.to_csv(save_path)
+		return df
 	else:
 		return df
 def process(ind_dir,ref_cmp=None,save=True):
+	#raise DeprecationWarning
 	raws=fu.file_list(ind_dir,file_format=".csv",pattern="raw")
 	objectives=fu.file_list(ind_dir,file_format=".csv",pattern="objectives")	
 
@@ -225,32 +230,41 @@ def get_run_files(ind_path,verbose=False):
 									critical=False,verbose=verbose)
 	pro_df=pd.read_csv(processed_file)
 
-	param_file=fu.assert_one_dim(fu.file_list(ind_path,file_format=".yaml",pattern="param"),\
-								critical=True,verbose=verbose)
-	dict_param=yaml.load(open(param_file))
-
-	
-	return dict_meta,pro_df,dict_param
+	return dict_meta,pro_df
 	
 def get_label(meta,count=None):
-	#print(meta)
+	try:
+		meta_params=meta["opt_params"]
+	except KeyError:
+		print(meta.keys())
+		raise KeyError
 	if "label" in meta.keys():
 		return meta["label"]
-	elif len(meta.keys())==1:
-		single_key=list(meta.keys())[0]
-		return round(meta[single_key],5)
-	elif len(meta.keys())>1 and count is not None:
+
+	elif len(meta_params)==1:
+		lab=list(meta_params.values())[0]
+		return lab
+	elif len(meta_params)>1 and count is not None:
 		return count
 def get_param_value(meta,param_name=None):
 	if param_name is None:
-		if len(meta.keys())==1:
-			param_name=list(meta.keys())[0]
-			return [param_name],[meta[param_name]] 
-		elif len(meta.keys())>1:
-			return list(meta.keys()),list(meta.values())
-		else:
-			raise('ValueError',meta)
-	else:
+		try:
+			meta_params=meta["opt_params"]
+		except KeyError:
+			print(meta.keys())
+			raise KeyError
+		#if len(meta_params)==1:
+		return list(meta_params.keys()),list(meta_params.values())
+		#if len(meta.keys())==1:
+		#	param_name=list(meta.keys())[0]
+		#	return [param_name],[meta[param_name]] 
+		#elif len(meta_params)>1:
+		#	return list(meta_params.keys()),list(meta_params.values())
+		#else:
+		#	print(meta)
+		#	raise ValueError
+	else: # not modified 
+		raise NotImplementedError
 		if type(param_name)==list:
 			vals=[]
 			for param in param_name:
@@ -308,18 +322,128 @@ def export_meta_params(indiv_dirs,ref_dir, disc_name, disc_params,save_path=None
 	print(df)
 	if save_path is not None:
 		df.to_csv(save_path)
-		
 
-def plot_with_success(indiv_dirs, metric, disc_name, disc_params, ref_dir=None,what="max_value",save=False):
+class runProcess:
+	metrics=["maxtime","corankle","corknee"]
+
+	def __init__(self,compare_kind,compare_files):
+		if compare_files is not None:
+			#print("\n[DEBUG] Init ref run process",compare_kind,compare_files)
+			self.ref=reference_compare(compare_kind,compare_files)
+			
+		else:
+			#print("\n[DEBUG] NO init ref run process")
+			self.ref=None
+		
+	def process_gen(self,gen_dir):
+		ind_dirs=fu.dir_list(gen_dir,pattern="ind")
+		cols=self.metrics.copy()
+		cols.append("uid")
+		scores=pd.DataFrame(index=pd.RangeIndex(len(ind_dirs)),
+							columns=cols)
+		try:
+			for ind,row_idx in zip(ind_dirs,scores.index):
+				dict_meta,pro_df=get_run_files(ind)
+				#print("\n[DEBUG] UID\n",dict_meta["uid"])
+				#print("\n[DEBUG] ILOC\n",scores.loc[row_idx])
+				#print("\n[DEBUG] slice\n",scores.loc[row_idx,self.metrics])
+				#print("\n[DEBUG] result\n",self.process_run(ind))
+				scores.loc[row_idx,self.metrics]=self.process_run(ind)
+				scores.loc[row_idx,"uid"]=dict_meta["uid"]
+		except Exception as e:
+			print(scores)
+			raise e
+		return scores
+
+
+	def process_run(self,ind_dir,save=False,verbose=False):
+
+		raws=fu.file_list(ind_dir,file_format=".csv",pattern="raw")
+
+		raws=fu.assert_one_dim(raws,critical=False)
+		if verbose:
+			print("\nProcessing",raws)
+		df=self.get_metrics(raws,verbose=verbose)
+		if save:
+			fu.assert_dir(ind_dir,should_be_empty=False)
+			save_path=os.path.join(ind_dir,"processed.csv")
+			print("Saving to",save_path)
+			df.to_csv(save_path)
+			return df
+		else:
+			#print("\n[DEBUG] Process run",df.values,"\n",df.values[0][1:-1])
+			return df.values[0][1:]
+	def get_metrics(self):
+		raise NotImplementedError
+
+class CppRunProcess(runProcess):
+	metrics=[	"maxtime",
+				"distance",
+				"mean_speed",
+				"energy",
+				"energy_to_dist",
+				"corankle",
+				"corhip",
+				"corknee"]
+	def __init__(self,compare_files):
+		#print("\n[DEBUG]Init CPP process",compare_files)
+		compare_kind="winter_to_cpp"
+
+		super().__init__(compare_kind,compare_files)
+	def get_metrics(self,raw_file,verbose=True):
+
+		#print("\nGetting metrics CPP")
+		raw_df=pd.read_csv(open(raw_file))
+		# Metrics computed during the run
+		metrics=pd.DataFrame(["value"])
+		metrics["maxtime"]=max(raw_df.index*TIME_STEP)
+		metrics["distance"]=raw_df["distance1_distance"].max()
+
+		metrics["mean_speed"]=metrics["distance"]/metrics["maxtime"]
+		metrics["energy"]=raw_df["energy1_energy"].max()
+		metrics["energy_to_dist"]=metrics["energy"]/metrics["distance"]
+
+		if self.ref is not None:
+			corr_dct=self.ref.get_all_corr_cpp(raw_df)
+			for met,corrval in corr_dct.items():
+				metrics["cor"+MAP_CPP_WINTER[met]]=corrval
+
+		#print(metrics)
+		return metrics
+
+class PythonRunProcess(runProcess):
+	def __init__(self,compare_files):
+		compare_kind="winter_to_python"
+		super().__init__(compare_kind,compare_files)
+	def get_metrics(self,raw_file,verbose=False):
+		raw_df=pd.read_csv(open(raw_file))
+		# Metrics computed during the run
+		metrics=pd.read_csv(open(objectives_file))
+		# Max simulation time
+		metrics["maxtime"]=max(raw_df.index*TIME_STEP)
+		# Energy as the sum of all activations
+		activation=raw_df.filter(like="act",axis=1)
+		metrics["energy"]=activation.sum(axis=1,skipna=True)
+		if self.ref is not None:
+			corr_dct=self.ref.get_all_corr(raw_df)
+			for met,corrval in corr_dct.items():
+				metrics["cor"+met]=corrval
+		if verbose:
+			print(metrics)
+		return metrics
+
+
+
+def plot_with_success(indiv_dirs, metric, disc_name, disc_params, ref_dir=None,what="max_value",save_path=None):
 	plot_qd_lst=[] # Contains (label,metric_value,param_value,discriminant)
 
 	for ind in indiv_dirs:
-		print("\n",os.path.basename(ind))
+		#print("\n",os.path.basename(ind))
 		ind_id=os.path.basename(ind)
-		meta, proc,param =get_run_files(ind)
-		lab = get_label(param)
+		meta, proc =get_run_files(ind)
+		lab = get_label(meta)
 		met = get_metric_value(proc, metric)
-		pname, param_value = get_param_value(param)
+		pname, param_value = get_param_value(meta)
 		disc = get_discriminant(proc, disc_name, disc_params)
 		if disc:
 			lab=str(round(lab,2))+"\n"+str(ind_id)
@@ -337,31 +461,50 @@ def plot_with_success(indiv_dirs, metric, disc_name, disc_params, ref_dir=None,w
 	fig_count = 0
 	fig=plt.figure(figsize=(len(sorted_qd_lst)*1.5,10))
 	ax=plt.axes()
-	
+	min_val=np.inf
+	max_val=-np.inf
 	label_lst=[]
 	for qd in sorted_qd_lst:
 		#print ("\n",qd[3])
 		fig_count += 1
 		label_lst.append(qd[0])
 		if qd[3]:
+			if qd[1]<min_val:
+				min_val=qd[1]
+			if qd[1]>max_val:
+				max_val=qd[1]
 			ax.bar(fig_count, qd[1])
 		else:
 			ax.plot(fig_count,0,marker='x', markersize=3, color="red")
 	plt.xlabel(pname)
+	if max_val>0:
+		ylim_up=max_val*1.05
+	else:
+		ylim_up=max_val*0.8
+
+	if min_val<0:
+		ylim_low=min_val*1.2
+	else:
+		ylim_low=min_val*0.8
+	#print("Values:\t",min_val,max_val)
+	#print("Limits:\t",ylim_low,ylim_up)
+	plt.ylim([ylim_low,ylim_up])
+
 	xt=np.arange(1,fig_count+1)
 	plt.xticks(xt,label_lst)
 	plt.ylabel(metric+" "+what)
 	plt.grid(axis='y')
-	if not save:
+	if save_path is None:
 		plt.show()
 	else:
 		
 		plt.rcParams.update({'font.size': 22})
 		fig_name=pname[0]+what+metric+".png"
-		p=os.path.join(SAVEPATH,fig_name)
+		p=os.path.join(save_path,fig_name)
 		#plt.tight_layout()
 		plt.savefig(p,dpi=300,transparent=False)
 		plt.close()
+
 
 
 if __name__ == '__main__':
@@ -402,10 +545,10 @@ if __name__ == '__main__':
 
 	elif mode=="process_and_save_cpp":
 		"""
-		python process_run.py process_and_save /data/prevel/runs/094_14:44 (ref_dir)
+		python process_run.py process_and_save_cpp /data/prevel/runs/094_14:44 (ref_dir)
 		"""
 		run_dir=param[0]
-		ref_kind="winter_cpp"
+		ref_kind="winter_to_cpp"
 		ref_raw=["/data/prevel/repos/biorob-semesterproject/data/winter_data/data_normal.csv"]
 		
 		ref=reference_compare(ref_kind,files=ref_raw)
@@ -416,7 +559,6 @@ if __name__ == '__main__':
 		if nb_gen>0:
 			prog_gen=1
 			for gen_dir in gen_dirs:
-				print("Gen",prog_gen,"/",nb_gen)
 				ind_dirs=fu.dir_list(gen_dir,pattern="ind")
 				nb_ind=len(ind_dirs)
 				prog_ind=1
@@ -445,22 +587,30 @@ if __name__ == '__main__':
 		velocity
 		4000
 		"""
+
+
 		run_dir = param[0]
 		#ref_dir=param[1]
-		metric="distance"
-		disc_param=0
+		metrics=["maxtime",	"distance",	"mean_speed",	"energy","energy_to_dist","corankle","corhip","corknee"]
+		disc_params=[0.0, 19.0, 19.0, 19.0, 19.0, 19.0,19.0,19.0]
 		do_save=True
+		gen_dirs=fu.dir_list(run_dir,"param")
 
-		gen_dirs=fu.dir_list(run_dir,"param",)
-		if len(gen_dirs)>0:
-			for gen_dir in gen_dirs:
-				ind_dirs=fu.dir_list(gen_dir,pattern="ind")
+		for metric,disc_param in zip(metrics,disc_params):
+			if do_save:
+				save_path=os.path.join(run_dir,"figures",metric)
+				fu.assert_dir(save_path)
+			else:
+				save_path=None
+			if len(gen_dirs)>0:
+				for gen_dir in gen_dirs:
+					ind_dirs=fu.dir_list(gen_dir,pattern="ind")
+					plot_with_success(	ind_dirs,metric,disc_name='maxtime',
+										disc_params=disc_param,what="max_value",save_path=save_path)
+			else:
+				ind_dirs=fu.dir_list(run_dir,pattern="ind")
 				plot_with_success(	ind_dirs,metric,disc_name='maxtime',
-									disc_params=disc_param,what="max_value",save=do_save)
-		else:
-			ind_dirs=fu.dir_list(run_dir,pattern="ind")
-			plot_with_success(	ind_dirs,metric,disc_name='maxtime',
-								disc_params=disc_param,what="max_value",save=do_save)
+									disc_params=disc_param,what="max_value",save_path=save_path)
 							
 	elif mode=="export_joined_df":
 		"""
