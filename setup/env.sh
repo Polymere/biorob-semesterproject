81be2ef5e5b9b561c7170547fd6dc12fcdea3e85
Paul Prevel <paul.prevel@epfl.ch>
2019-05-24 20:22:36 +0200

    updt

--- setup/env.sh
+++ setup/env.sh
@@ -4,7 +4,7 @@ if [[ $ENVS = *"bio"* ]]; then
    source activate bio
 else 
    echo "Installing conda env "
-	conda env create -f biorob_proj_config.yml
+	conda create --name bio --file biorob_proj_config.yml
 	source activate bio
    exit
 fi;
