a13c687841ff589be5cc9697af221b8e1e4190fa
paul prevel <paul.prevel@epfl.ch>
2019-06-06 18:00:36 +0200

    added working pso

--- src/optimisation/optimizers.py
+++ src/optimisation/optimizers.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import os
 import time
-
+from copy import copy
 import warnings
 warnings.filterwarnings('ignore') # pandas warning a utter trash
 
@@ -28,8 +28,8 @@ class Optimizer:
 				setattr(self, arg_name, arg_value)
 		if LOG_LEVEL<=LOG_INFO:
 			print("\n[INFO]Optimizer ",self.__class__.__name__," initialized with\n",self.__dict__)
-
-		self.flatten_params()
+		if self.dofs is not None:
+			self.flatten_params()
 	def check_bound(self,population):
 		for index, row in population.iterrows():
 			if LOG_LEVEL<=LOG_DEBUG:
@@ -75,30 +75,51 @@ class Optimizer:
 
 		self.flatten_params()
 		gen_counter=0
-		uids=["gen"+str(gen_counter)+"ind"+str(i+1) for i in range(self.nb_ind)]
+		uids=self.get_uid(nb_gen,self.nb_ind)
 		pop_df=pd.DataFrame(index=uids,columns=self.dofs.keys())
-		vals=(self._bound_high-self._bound_low)*np.random.rand(self.nb_ind,len(self._bound_low)) - self._bound_low
+
+		vals=np.random.uniform(low=self._bound_low,high=self._bound_high,size=(self.nb_ind,len(self.dofs)))
 		pop_df[:]=vals
-		self.current_pop=pop_df
+		current_pop=pop_df
+		performance_df=None
 		while gen_counter<nb_gen:
-			eval_pop=self.bench_eval(self.current_pop)
+			eval_pop=self.bench_eval(current_pop)
+			nrow={}
+			for fit in eval_pop.filter(like="fit").columns:
+				nrow[fit+"_std"]=eval_pop[fit].std()
+				nrow[fit+"_mean"]=eval_pop[fit].mean()
+				nrow[fit+"_best"]=eval_pop[fit].max()
+			if performance_df is None:
+				performance_df=pd.DataFrame(index=pd.RangeIndex(nb_gen),columns=nrow.keys())
+
+			performance_df.iloc[gen_counter]=nrow
+			if self.is_single_obj:
+				eval_pop["fit"]=eval_pop.filter(like="fit").sum(1)
+
 			parents=self.select(self.sort_pop(eval_pop))
+			print("\nParents\n",parents)
 			if gen_counter%10==0:
 				print(parents)
 			gen_counter+=1
-			self.current_pop=self.get_next_gen(eval_pop,gen_counter)
-
+			current_pop=self.get_next_gen(parents,gen_counter)
+		performance_df.to_csv("perf_"+problem+self.__class__.__name__+".csv")
 	def _eval_rastrigin(self,pop):
-		fit=10*self.n+(pop.values**2-10*np.cos(pop.values*2*np.pi)).sum(1)
+		fit=10*self.n+(pop.values**2-10*np.cos(pop.values.astype(np.float32)*2*np.pi)).sum(1)
 		pop["fit"]= - fit
 		return pop
 	def _eval_sphere(self,pop):
+		print("\n---------------------\n")
+		print(pop)
+		print(pop.values)
+		print("\n---------------------\n")
 		fit=(pop.values**2).sum(1)
 		pop["fit"]=-fit
+		#print("EVAL SPHERE\n",pop)
 		return pop
 	def _eval_fonseca_fleming(self,pop):
-		f1= 1 - np.exp( ( - ( pop.values - 1 / np.sqrt( self.n ) )**2 ).sum(1) )
-		f2= 1 - np.exp( ( - ( pop.values + 1 / np.sqrt( self.n ) )**2 ).sum(1) )
+
+		f1= 1 - np.exp( ( - ( pop.values.astype(np.float32) - 1 / np.sqrt( self.n ) )**2 ).sum(1) )
+		f2= 1 - np.exp( ( - ( pop.values.astype(np.float32) + 1 / np.sqrt( self.n ) )**2 ).sum(1) )
 		pop["fit_1"]= - f1
 		pop["fit_2"]= - f2
 		return pop
@@ -121,20 +142,116 @@ class Optimizer:
 		return eval_pop.loc[srt.index,:]
 	def select(self,sorted_pop):
 		return sorted_pop.head(self.nb_parents)#["uid"]
-	def set_uid(self,gen_nb,new_gen):
-		uids=["gen"+str(gen_nb)+"ind"+str(i+1) for i in range(self.nb_ind)]
-		new_gen.index=uids
-		return new_gen
 
 	def get_next_gen(self,parents,gen_nb):
 		raise NotImplementedError
 
 class PSOptimizer(Optimizer):
-
+	vel_range=None
+	c1=0.1
+	c2=0.3
 	def __init__(self, arg):
 		super(PSOptimizer, self).__init__(arg)
 		self.is_single_obj=True
-		raise NotImplementedError
+		if self.nb_ind!=self.nb_parents:
+			raise ValueError
+		else:
+			self.nb_particules=self.nb_parents
+		self._speed=None
+		self._global_best_fit=None
+		self._global_best_pos=None
+	def particles_init(self,eval_pop):
+
+		uids=self.get_uid()
+		fit=eval_pop.filter(like="fit")
+		fitnesses=fit.sum(1)
+		positions=eval_pop.drop(columns=fit.columns)
+		self._speed={}
+		self._positions={}
+		self._best_position={}
+		self._best_fit={}
+		for part,idx in zip(uids,range(self.nb_particules)):
+			self._speed[part]=np.random.uniform(low=self.vel_range[0],high=self.vel_range[1],size=len(self.dofs))
+			cfit=fit.iloc[idx].values[0]
+			cpos=positions.iloc[idx].to_dict()
+			if self._global_best_fit is None or self._global_best_fit<cfit:
+				self._global_best_fit=cfit
+				self._global_best_pos=cpos
+			self._positions[part]=cpos
+			self._best_position[part]=cpos
+			self._best_fit[part]=cfit
+
+		print("\nSpeeds\n",self._speed)
+		print("\nPositions\n",self._positions)
+		print("\nParticle best\n",self._best_fit)
+
+	def sort_pop(self,eval_pop):
+		if self._speed is None:
+			self.particles_init(eval_pop)
+		return eval_pop.sort_values("fit",ascending=False)
+	def select(self,sort_pop):
+		return sort_pop
+
+	def get_uid(self,tmp1=None,tmp2=None):
+		return["particule"+str(i+1) for i in range(self.nb_particules)]
+	def _dist(self,pos1,pos2):
+		dist=np.zeros((len(pos1)))
+		#print(pos1,"\n",pos2)
+		for dim, idx in zip(pos1.keys(),range(len(pos1))):
+			d=pos1[dim]-pos2[dim]
+			dist[idx]=d
+		return dist
+
+	def _inc(self,dct1,val):
+
+		nb_dim=len(dct1.keys())
+		for dim,dim_idx in zip(dct1.keys(),range(nb_dim)):
+			dct1[dim]=dct1[dim]+val[dim_idx]
+		return dct1
+
+	def updt_velocities(self):
+		for part in self._speed.keys():
+			f1=self.c1*np.random.uniform(0,1)*self._dist(self._best_position[part],self._positions[part])
+			f2=self.c2*np.random.uniform(0,1)*self._dist(self._global_best_pos,self._positions[part])
+			
+			self._speed[part]=self._speed[part]+f1+f2
+			#if self._speed[part]>self.vel_range[0]:
+			self._speed[part][self._speed[part]<self.vel_range[0]]=self.vel_range[0]
+			self._speed[part][self._speed[part]>self.vel_range[1]]=self.vel_range[1]
+		print("VEL UPDT\n",f1,"\n",f2,"\n",self._speed[part])
+	def updt_positions(self):
+		for part in self._positions.keys():
+			self._positions[part]=self._inc(self._positions[part], self._speed[part])
+	def updt_best(self,sorted_pop):
+		#print("Updt best for\n",sorted_pop)
+		#print("\n Initial best pos",self._best_position)
+		fit=sorted_pop.filter(like="fit")
+		positions=sorted_pop.drop(columns=fit.columns)
+		#print("Positions\n",positions)
+		for part in self._positions.keys():
+			cfit=sorted_pop.loc[part].fit
+			if cfit>self._best_fit[part]:
+				if LOG_LEVEL<=LOG_DEBUG:
+					print("\n[DEBUG]Updt best for\n", part,cfit)
+					print("\n[DEBUG]Particule:\t",part)
+					print("\n[DEBUG]Stored position:\t",self._positions[part])
+					print("\n[DEBUG]Received position:\t",positions.loc[part])
+				self._best_fit[part]=cfit
+				self._best_position[part]=copy(self._positions[part])
+			if cfit>self._global_best_fit:
+				self._global_best_fit=cfit
+				self._global_best_pos=copy(self._positions[part])
+		#print("\n Final best pos",self._best_position)
+		return
+	def get_next_gen(self,sorted_pop,gen_counter):
+		self.updt_velocities()
+		self.updt_best(sorted_pop)
+		self.updt_positions()
+		pos_df=pd.DataFrame(self._positions)
+		print("\nBest:\t",self._global_best_fit,"\n\t",self._global_best_pos)
+
+		return pos_df.T
+
 
 
 class GAOptimizer(Optimizer):
@@ -152,14 +269,17 @@ class GAOptimizer(Optimizer):
 			return pop
 		else:
 			raise NotImplementedError
-
+	def get_uid(self,gen_nb,new_gen):
+		return ["gen"+str(gen_nb)+"ind"+str(i+1) for i in range(self.nb_ind)]
+	def set_uid(self,gen_nb,new_gen):
+		new_gen.index=self.get_uid(gen_nb, new_gen)
+		return new_gen
 	def get_next_gen(self,parents,gen_nb):
 		new_gen=self.check_bound(self.cross_and_mutate(parents))
 		return self.set_uid(gen_nb, new_gen)
 
 	def cross_and_mutate(self,selected_parents):
 		nb_dofs=len(self.dofs)
-
 		couples=np.random.randint(0, len(selected_parents.index), (2,self.nb_ind))
 		if LOG_LEVEL<=LOG_DEBUG:
 			print("\n[DEBUG]selected_parents\n",selected_parents)
@@ -286,11 +406,30 @@ class NSGAIIOptimizer(GAOptimizer):
 		return pop_with_fronts
 
 if __name__ == '__main__':
-	pars={"mut_amp":0.5,
-	"mut_rate":0.9,
-	"cross_rate":0.1,
-	"nb_ind":100,
-	"nb_parents":10}
+	arg=sys.argv[1]
+	if arg=="PSO":
+		pars={"vel_range":[-0.5,0.5],
+		"nb_ind":50,
+		"nb_parents":50}
 
-	opti=GAOptimizer(pars)
-	opti.benchmark("fonseca_fleming", 100, 2)
+		opti=PSOptimizer(pars)
+	elif arg=="GA":
+		pars={	"mut_amp":0.1,
+				"mut_rate":0.5,
+				"cross_rate": 0.5,
+				"nb_ind":50,
+				"nb_parents":5}
+
+		opti=GAOptimizer(pars)
+	elif arg=="NSGAII":
+		pars={	"mut_amp":0.1,
+				"mut_rate":0.5,
+				"cross_rate": 0.5,
+				"nb_ind":50,
+				"nb_parents":5}
+
+		opti=NSGAIIOptimizer(pars)
+	else:
+		print(arg)
+		raise KeyError
+	opti.benchmark("fonseca_fleming", 20, 2)
